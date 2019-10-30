03d8ded898c221e56b5a30fc6dd26894f449d85f
samgomena <samiam4612@gmail.com>
2019-10-30 07:25:22 -0700

    feat: Add trivial index js and styles

--- 465/project1/main.js
+++ 465/project1/main.js
@@ -7,13 +7,19 @@ const states = {
     },
 };
 
+/**
+ * Get a randomly generated number in the range [`min`, `max`]. I.e. inclusively.
+ * 
+ * @param {int} min a lower bound for the range of numbers to choose from
+ * @param {int} max an upper bound for the range of numbers to choose from
+ */
 function get_random_between(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-;;(async function fetchGradientData() {
++(async function fetchGradientData() {
 
     const gradient_json = await fetch("assets/gradients.json", 
     {
@@ -38,11 +44,6 @@ function get_random_between(min, max) {
             get_random_between(0, gradient_data.length - 1)
         ].gradient,
     ]
-    
-    // console.log(gradient_data);
-    console.log(
-        default_state
-    )
 
     const granimInstance = new Granim({
         element: '#canvas',
@@ -53,47 +54,9 @@ function get_random_between(min, max) {
         stateTransitionSpeed: 500,
         states : {
             "default-state": {
-                // gradients: [
-                //     ['#B3FFAB', '#12FFF7'],
-                //     ['#ADD100', '#7B920A'],
-                //     ['#1A2980', '#26D0CE']
-                // ],
                 gradients: default_state,
                 transitionSpeed: 10000
             },
-            "violet-state": {
-                gradients: [
-                    ['#9D50BB', '#6E48AA'],
-                    ['#4776E6', '#8E54E9']
-                ],
-                transitionSpeed: 2000
-            },
-            "orange-state": {
-                gradients: [ ['#FF4E50', '#F9D423'] ],
-                loop: false
-            }
         }
     });
 })()
-
-// With jQuery
-$('#default-state-cta').on('click', function(event) {
-    event.preventDefault();
-    granimInstance.changeState('default-state');
-    setClass('#default-state-cta')
-});
-$('#violet-state-cta').on('click', function(event) {
-    event.preventDefault();
-    granimInstance.changeState('violet-state');
-    setClass('#violet-state-cta')
-});
-$('#orange-state-cta').on('click', function(event) {
-    event.preventDefault();
-    granimInstance.changeState('orange-state');
-    setClass('#orange-state-cta')
-});
-
-function setClass(element) {
-    $('.canvas-interactive-wrapper a').removeClass('active');
-    $(element).addClass('active');
-};
