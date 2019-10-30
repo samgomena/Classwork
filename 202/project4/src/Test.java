61a165fcceb2d118f580951a9353a48a8a7ccb3a
samgomena <samiam4612@gmail.com>
2019-10-29 18:55:57 -0700

    feat: Add leftovers from 202

---
+++ 202/project4/src/Test.java
@@ -0,0 +1,20 @@
+public class Test {
+
+//    public static boolean testTree() {
+//        Tree tree = new Tree();
+//
+//        //        boolean added_all = tree.insert(10) && tree.insert(13) && tree.insert(5) && tree.insert(69) && tree.insert(2) && tree.insert(7) && /*tree.insert(23) && */ tree.insert(11) && tree.insert(12) && tree.insert(68) && tree.insert(69) && tree.insert(11);
+//        boolean added_all = tree.insert(7) && tree.insert(4) && tree.insert(5) && tree.insert(6) && tree.insert(20) && tree.insert(19) && tree.insert(18) && tree.insert(1) && tree.insert(2) && tree.insert(17) && tree.insert(16) && tree.insert(8);
+//        int count = tree.display();
+//        System.out.println("Added all: " + added_all + "\nTherefore we have " + count + " nodes.");
+//
+////        System.out.println("Root: " + tree.retrieve());
+//        System.out.println("retrieve(8) = " + tree.retrieve(8));
+//        System.out.println("Height: " + tree.height());
+//        System.out.println("Level Order:");
+//        tree.printLevelOrder();
+//        System.out.println();
+//        tree.display();
+//        return added_all;
+//    }
+}
